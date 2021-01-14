9b802ca46063fe6aff4613742688f453d5d97207
EC2 Default User <ec2-user@ip-172-31-72-246.ec2.internal>
2021-01-14 14:37:16 +0000

    food-list update

--- app/controllers/admin/admin_foods_controller.rb
+++ app/controllers/admin/admin_foods_controller.rb
@@ -1,21 +1,35 @@
 class Admin::AdminFoodsController < ApplicationController
-  
+
   def index
-    @admin_foods = AdminFood.all
     @admin_food = AdminFood.new
-    @food_genres = FoodGenre.all
-    @food_genre = FoodGenre.new
+    @food_genres = FoodGenre.includes(:admin_foods).all
   end
 
   def create
+    @admin_food = AdminFood.new(admin_food_params)
+    @admin_food.save
+    redirect_to admin_admin_foods_path
   end
 
   def edit
+    @admin_food = AdminFood.find(params[:id])
+    @food_genres = FoodGenre.includes(:admin_foods).all
   end
 
   def update
+    @admin_food = AdminFood.find(params[:id])
+    @admin_food.update(admin_food_params)
+    redirect_to edit_admin_admin_food_path(@admin_food)
   end
 
   def destroy
+    @admin_food = Admin.find(params[:id])
+    @admin_food.destroy
+    redirect_to edit_admin_admin_food_path(@admin_food)
+  end
+
+  private
+  def admin_food_params
+    params.require(:admin_food).permit(:food_genre_id, :name, :unit)
   end
 end
