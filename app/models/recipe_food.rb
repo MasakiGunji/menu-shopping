9c152f830cc36ab23e8305eab87ff22d8bcbdea7
EC2 Default User <ec2-user@ip-172-31-72-246.ec2.internal>
2021-01-28 15:02:36 +0000

    modfy errors 1-28

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,12 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
+
+  def division
+    numerator/denominator
+  end
+
+  def remainder
+    numerator%denominator
+  end
 end
